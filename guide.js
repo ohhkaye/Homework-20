a83f72af6f77fea99c762cf1c8697388096a2371
ohhkaye <hetrick.haley@gmail.com>
2023-05-13 23:19:45 -0400

    First iteration

---
+++ guide.js
@@ -0,0 +1,14 @@
+console.log("Hello world");
+
+$(document).ready(function(){
+    $('.top').hide();
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 200) {
+          //  perform this if true
+          $('.top').fadeIn();
+        } else {
+            // perform this if not true (or false)
+            $('.top').fadeOut();
+        }
+    });
+});
